0898b4770b105c72f93bb318baadd11fa458e327
Josiah Parappally <josiahparappally@gmail.com>
2020-05-27 22:15:35 -0400

    changed navbar routing, class components to functional, and added splash page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import Navbar from "./components/navbar.component.js"
-import shoesList from "./components/shoes-list.component.js";
-import editShoe from "./components/edit-shoe.component.js";
-import createShoe from "./components/create-shoe.component.js";
+import ShoesList from "./components/shoes-list.component.js";
+import EditShoe from "./components/edit-shoe.component.js";
+import CreateShoe from "./components/create-shoe.component.js";
 import CreateUser from "./components/create-user.component.js";
+import Splash from "./components/splash.component.js";
 
 function App() {
   return (
@@ -14,9 +15,10 @@ function App() {
     <div className="container">
       <Navbar />
         <br/>
-        <Route path="/" exact component={shoesList} />
-        <Route path="/edit/:id" component={editShoe} /> 
-        <Route path="/create" component={createShoe} />
+        <Route path="/" exact component={Splash} />
+        <Route path="/shoes" exact component={ShoesList} />
+        <Route path="/edit/:id" component={EditShoe} /> 
+        <Route path="/create" component={CreateShoe} />
         <Route path="/user" component={CreateUser} />
     </div>
     </Router>
---
+++ frontend/src/components/splash.component.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+function Splash(props) {
+    return <h1>You are on the Splash component</h1>;
+}
+
+export default Splash;
