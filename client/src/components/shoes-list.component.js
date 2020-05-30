7284ce7aa453129c61b24c90c856f0ead67d90f6
Prannoy Lal <prannoylal@gmail.com>
2020-05-30 18:34:13 -0400

    #7eha5q added material-ui card and basic hooks setup

--- client/src/components/shoes-list.component.js
+++ client/src/components/shoes-list.component.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, Grid, makeStyles } from '@material-ui/core'
+import axios from 'axios';
+
+const useStyles = makeStyles({
+    gridContainer: {
+        paddingLeft: '20px',
+        paddingRight: '20px'
+    }
+});
 
 function ShoesList(props) {
-    return <h1>You are on the Shoes List component</h1>;
+    const classes = useStyles();
+    const [shoes, updateShoes] = useState([]);
+
+    useEffect(() => {
+        async function getShoes(){
+            const shoes = await axios.get('localhost:5000/shoes/');
+            updateShoes(shoes);
+        }
+        getShoes();
+    });
+
+    for (const shoe in shoes) {
+        console.log(shoe.size);
+    }
+    
+    return (
+        <div>
+            <h1>You are on the Shoes List component</h1>
+            <h5>Take a look at everything we have to offer!</h5>
+            <Grid container spacing={4} className={classes.gridContainer}>
+                <Grid item xs={4}>
+                    <Card />
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
 
 export default ShoesList;
