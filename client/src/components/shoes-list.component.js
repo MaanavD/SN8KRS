d0bab60b880af80604c9c81e4065dbb400d06d09
Prannoy Lal <prannoylal@gmail.com>
2020-06-08 20:00:04 -0400

    added cards and shoe card component

--- client/src/components/shoes-list.component.js
+++ client/src/components/shoes-list.component.js
@@ -1,55 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import { Card, CardHeader, CardContent, Grid, makeStyles, Typography } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 import axios from 'axios';
-
-const useStyles = makeStyles({
-    gridContainer: {
-        paddingLeft: '20px',
-        paddingRight: '20px'
-    }
-});
+import ShoeCard from './shoe-card.component';
 
 function ShoesList(props) {
-    const classes = useStyles();
     const [shoes, updateShoes] = useState([]);
 
-    async function getShoes() {
+
+    useEffect(async () => {
         let { data } = await axios.get('http://localhost:5000/shoes');
         updateShoes(data);
-    }
-
-    useEffect(() => {
-        getShoes();
     }, []);
 
-    for (const shoe in shoes) {
-        console.log(shoe);
-    }
+    console.log(shoes);
+
+    const shoeItems = shoes.map((shoe) => (
+        <Grid item xs={4}>
+            <ShoeCard shoe={shoe}/>
+        </Grid>
+    ));
 
     return (
         <div>
             <h1>You are on the Shoes List component</h1>
             <h5>Take a look at everything we have to offer!</h5>
-            <Grid container spacing={4} className={classes.gridContainer}>
-                <Grid item xs={2}>
-                    <Card>
-                        <CardContent>
-                            <Typography className={classes.title}>
-                                {/* {shoeArray[0].brand} */}
-                            </Typography>
-                        </CardContent>
-                    </Card>
-
-                    {/* {Object.keys(shoes).map(shoe => (
-                        <Card>
-                            <CardContent>
-                                <Typography className={classes.title}>
-                                    {shoe.brand}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    ))} */}
-                </Grid>
+            <br />
+            <Grid container spacing={2}>
+                    {shoeItems}
             </Grid>
         </div>
     );
