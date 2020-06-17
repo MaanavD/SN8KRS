a1a868f6376ee7a9ffe3cdee8301995180f54d47
Josiah Parappally <josiahparappally@gmail.com>
2020-06-16 22:46:44 -0400

    updated splash page with temporary image, and fixed splash text

--- client/src/components/splash.component.js
+++ client/src/components/splash.component.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import splash from '../assets/splash.jpg'
 
 function Splash(props) {
-    return <h1>You are on the Splash component</h1>;
+    return (
+        <div>
+            <h1> Welcome to SN8KRS, a peer to peer shoe trading marketplace </h1>
+            <img src={splash} alt="Logo" />
+        </div>
+    )
 }
 
 export default Splash;
