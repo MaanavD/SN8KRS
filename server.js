8a88a6cd6f67bb05495a15a6e11b10a372ca0a75
Josiah Parappally <josiahparappally@gmail.com>
2020-06-27 15:48:48 -0400

    removed router

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = socketio(server);
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(router);
+// app.use(router);
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "client", "build")))
 
