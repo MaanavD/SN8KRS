8e2826fd3e142a662fa6160f632f2197b635e13f
Maanav Dalal <dalalm1@mcmaster.ca>
2020-08-19 13:20:23 -0400

    refactored frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,25 @@
 import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route} from "react-router-dom";
-
-import Navbar from "./components/navbar.component.js"
-import ShoesList from "./components/shoes-list.component.js";
-import EditShoe from "./components/edit-shoe.component.js";
-import CreateShoe from "./components/create-shoe.component.js";
-import CreateUser from "./components/create-user.component.js";
-import Splash from "./components/splash.component.js";
-import Chat from './components/Chat/chat.component.js';
-import Join from './components/Join/join.component.js';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-    <div className="container">
-      <Navbar />
-        <br/>
-        <Route path="/" exact component={Splash} />
-        <Route path="/shoes" exact component={ShoesList} />
-        <Route path="/edit/:id" component={EditShoe} /> 
-        <Route path="/create" component={CreateShoe} />
-        <Route path="/user" component={CreateUser} />
-        <Route path="/chat" component={Chat} />
-        <Route path="/join" component={Join} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    </Router>
   );
 }
 
--- client/src/components/edit-shoe.component.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function EditShoe(props) {
-    return <h1>You are on the Edit Shoe component</h1>;
-}
-
-export default EditShoe;
