c6e6ca6f7cc5b794690624f87fe0dc4d23b5477f
Prannoy Lal <prannoylal@gmail.com>
2020-08-26 13:26:27 -0400

    updated async routes and followed database schema

--- models/shoe.model.js
+++ models/shoe.model.js
@@ -3,13 +3,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const shoeSchema = new Schema({
-    brand: {type: String, required: true, trim: true},
-    colorway: {type: String, required: true, trim: true},
-    modelName: {type: String, required: true, trim: true},
-    offerMax: {type: Number, required: true},
-    offerMin: {type: Number, required: true},
     size: {type: Number, required: true},
+    gender: {type: String, required: true, trim: true},
+    brand: {type: String, required: true, trim: true},
+    modelName: {type: String, required: true, trim: true},
+    colorway: {type: String, required: true, trim: true},
+    offerMin: {type: Number, required: true},
+    offerMax: {type: Number, required: true},
     sellerUsername: {type: String, required: true, trim: true},
+    picture: {type: BinData, required: true, trim: true},
   }, {
     timestamps: true,
 });
--- routes/shoes.js
+++ routes/shoes.js
@@ -1,72 +1,139 @@
 const router = require('express').Router();
+const multer = require('multer');
+const GridFSStorage = require('multer-gridfs-storage');
 let Shoe = require('../models/shoe.model');
 
-router.route('/').get((req, res) => {
-    Shoe.find()
-      .then(shoes => res.json(shoes))
-      .catch(err => res.status(400).json('Error: ' + err));
+// Create storage engine
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err)
+        }
+        const filename = file.originalname
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads',
+        }
+        resolve(fileInfo)
+      })
+    })
+  },
 });
 
-router.route('/add').post((req, res) => {
-    const colorway = req.body.colorway;
-    const brand = req.body.brand;
-    const modelName = req.body.modelName;
-    const offerMax = Number(req.body.offerMax);
-    const offerMin = Number(req.body.offerMin);
-    const size = Number(req.body.size);
-    const sellerUsername = req.body.sellerUsername;
-  
-    const newShoe = new Shoe({
-      brand,
-      colorway,
-      modelName,
-      offerMax, 
-      offerMin, 
+const upload = multer({ storage });
+
+router.get("/", async (req, res) => {
+  try {
+    let shoes = await Shoe.find();
+    res.json(shoes);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// router.route('/').get((req, res) => {
+//     Shoe.find()
+//       .then(shoes => res.json(shoes))
+//       .catch(err => res.status(400).json('Error: ' + err));
+// });
+
+router.post("/add", async (req, res) => {
+  try {
+    let { size, gender, brand, modelName, colorway, offerMin, offerMax, sellerUsername } = req.body;
+    size = Number(size);
+    offerMin = Number(offerMin);
+    offerMax = Number(offerMax);
+    // const picture = req.body.picture;
+    let newShoe = new Shoe({
       size,
+      gender,
+      brand,
+      modelName,
+      colorway,
+      offerMin,
+      offerMax,
       sellerUsername
     });
-  
-    newShoe.save()
-      .then(() => res.json('Shoe added!'))
-      .catch(err => res.status(400).json('Error: ' + err));
+    await newShoe.save();
+    res.json("Shoe added");
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
-router.route('/:id').get((req, res) => {
-  Shoe.findById(req.params.id)
-    .then(shoe => res.json(shoe))
-    .catch(err => res.status(400).json('Error: ' + err));
+router.get("/:id", async (req, res) => {
+  try {
+    let shoe = await Shoe.findById(req.params.id);
+    res.json(shoe);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
-router.route('/:id').delete((req, res) => {
-  Shoe.findByIdAndDelete(req.params.id)
-    .then(() => res.json('Shoe deleted.'))
-    .catch(err => res.status(400).json('Error: ' + err));
+// router.route('/:id').get((req, res) => {
+//   Shoe.findById(req.params.id)
+//     .then(shoe => res.json(shoe))
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await Shoe.findByIdAndDelete(req.params.id);
+    res.json("Shoe deleted");
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
-router.route('/update/:id').post((req, res) => {
-  Shoe.findById(req.params.id)
-    .then(shoe => {
+// router.route('/:id').delete((req, res) => {
+//   Shoe.findByIdAndDelete(req.params.id)
+//     .then(() => res.json('Shoe deleted.'))
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
 
-      shoe.colorway = req.body.colorway;
-      shoe.brand = req.body.brand;
-      shoe.modelName = req.body.modelName;
-      shoe.offerMax = Number(req.body.offerMin);
-      shoe.offerMin = Number(req.body.offerMin);
-      shoe.size = Number(req.body.size);
-      shoe.sellerUsername = req.body.sellerUsername;
+router.post("/update/:id", async (req, res) => {
+  try {
+    let shoe = await Shoe.findById(req.params.id);
+    [ shoe.size, shoe.gender, shoe.brand, shoe.modelName, shoe.colorway, shoe.offerMin, shoe.offerMax, shoe.sellerUsername ] = 
+    [ Number(req.body.size), req.body.gender, req.body.modelName, Number(req.body.offerMin), Number(req.body.offerMax), req.body.sellerUsername ];
+    await shoe.save();
+    res.json("Shoe updated");
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 
-      shoe.save()
-        .then(() => res.json('Shoe updated!'))
-        .catch(err => res.status(400).json('Error: ' + err));
-    })
-    .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/user/:id').get((req, res) => {
-  Shoe.find({sellerUsername: req.params.id})
-    .then(shoe => res.json(shoe))
-    .catch(err => res.status(400).json('Error: ' + err));
+// router.route('/update/:id').post((req, res) => {
+//   Shoe.findById(req.params.id)
+//     .then(shoe => {
+//       [ shoe.size, shoe.gender, shoe.brand, shoe.modelName, shoe.colorway, shoe.offerMin, shoe.offerMax, shoe.sellerUsername ] = 
+//       [ Number(req.body.size), req.body.gender, req.body.modelName, Number(req.body.offerMin), Number(req.body.offerMax), req.body.sellerUsername ];
+
+//       shoe.save()
+//         .then(() => res.json('Shoe updated!'))
+//         .catch(err => res.status(400).json('Error: ' + err));
+//     })
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
+
+router.get("/shoe/:id", async (req, res) => {
+  try {
+    let shoes = await Shoe.find({ shoeId: req.params.id });
+    res.json(shoes);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
+
+// router.route('/user/:id').get((req, res) => {
+//   Shoe.find({sellerUsername: req.params.id})
+//     .then(shoe => res.json(shoe))
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
 
 
 module.exports = router;
